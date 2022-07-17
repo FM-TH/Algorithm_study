6d64e77d4166d31669749ad7a26f00ef2d19ea49
tahara <taharafumiya0219@gmail.com>
2022-07-17 15:51:04 +0900

    test

---
+++ part4/memo_test.py
@@ -0,0 +1,13 @@
+def test1():
+    for i in range(5):
+        x = i + 1
+        y = i + 2
+        z = i + 3
+        print(x, y, z)
+
+
+def test2():
+    list = []
+    for i in range(5):
+        list.append(i)
+    for
